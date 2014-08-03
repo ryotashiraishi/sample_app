e4f12c8aef508bf29cbcf8be0a591df6f5ed28ca
ryotashiraishi <mentaikobai@gmail.com>
2014-08-03 20:16:54 +0900

    Finish sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password # <-を上部に持ってくる意味は??
-  before_save { email.downcase! }
+  before_save { self.email = email.downcase }
+  before_create  :create_remember_token 
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i
@@ -8,4 +9,19 @@ class User < ActiveRecord::Base
                     format: {with: VALID_EMAIL_REGEX}, 
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+  
+  private
+
+      def create_remember_token
+        # トークンを作成する。
+        self.remember_token = User.encrypt(User.new_remember_token)
+      end
 end
